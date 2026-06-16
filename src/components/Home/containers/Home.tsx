74da002756cb57f9daaacda64c170cbc97d5930a
David Hormiga Afonso <davidhormigafonso@gmail.com>
2026-06-16 15:11:40 +0200

    refactor(Home): simplify data fetching by using static test prop

--- src/components/Home/containers/Home.tsx
+++ src/components/Home/containers/Home.tsx
@@ -1,20 +1,8 @@
 import '../components/HomeComponent.scss'
 import HomeComponent from '../components/HomeComponent'
-import { useEffect, useState } from 'react'
 
 const Home = () => {
-	const [data, setData] = useState('')
-
-	const fetchSuperImportantData = async () => {
-		// Here your amazing fetching data logic
-		setData('Fetched Data')
-	}
-
-	useEffect(() => {
-		fetchSuperImportantData()
-	}, [])
-
-	return <HomeComponent test={data} />
+	return <HomeComponent test='Fetched Data' />
 }
 
 export default Home
